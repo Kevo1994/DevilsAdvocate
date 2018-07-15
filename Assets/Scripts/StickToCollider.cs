00176fe63c2d8c8cf3fa2d89aeaaacf47331464b
Kevo1994 <kfj26@drexel.edu>
2018-07-15 15:21:19 -0400

    VR Player Controller and Stick functionality

---
+++ Assets/Scripts/StickToCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickToCollider : MonoBehaviour {
+
+    //public GameObject ColliderObject;
+
+    private Transform BlockerLocation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Devil"))
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Devil"))
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
